bde7197023367e43a4ac885772424fef98ff9245
Jay Reddy <jayreddy@gmail.com>
2023-10-24 11:00:38 -0700

    inverts if conditions and runs gofumpt

--- dll/deque.go
+++ dll/deque.go
@@ -1,18 +1,14 @@
-package dll               
-
-import (
-	"fmt"
-)
+package dll
 
 type node struct {
 	value interface{}
-	next *node
-	prev *node
+	next  *node
+	prev  *node
 }
 
 type Dll struct {
-	head *node
-	tail *node
+	head   *node
+	tail   *node
 	length int
 }
 
@@ -20,44 +16,40 @@ func New() *Dll {
 	return &Dll{}
 }
 
-func (dll *Dll) Len() int{
+func (dll *Dll) Len() int {
 	return dll.length
 }
 
 func (dll *Dll) PushRight(val interface{}) {
 	node := &node{
 		value: val,
-		next: nil,
-		prev: dll.tail,
+		next:  nil,
+		prev:  dll.tail,
 	}
 
-	if dll.tail != nil {
+	if dll.length != 0 {
 		dll.tail.next = node
-	}
-
-	if dll.length == 0 {
+		dll.tail = node
+	} else {
 		dll.head = node
+		dll.tail = node
 	}
-
-	dll.tail = node
 	dll.length++
 }
 
 func (dll *Dll) PushLeft(val interface{}) {
 	node := &node{
 		value: val,
-		next: dll.head,
-		prev: nil,
+		next:  dll.head,
+		prev:  nil,
 	}
 
-	if dll.head != nil {
+	if dll.length != 0 {
 		dll.head.prev = node
-	}	
-
-	if dll.length == 0 {
+		dll.head = node
+	} else {
+		dll.head = node
 		dll.tail = node
 	}
-
-	dll.head = node
 	dll.length++
 }

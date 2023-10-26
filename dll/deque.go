7c9da988bdc37f4d8b0047803ad56fc38c7ee43d
Jay Reddy <jayreddy@gmail.com>
2023-10-25 20:31:38 -0700

    inits popLeft

--- dll/deque.go
+++ dll/deque.go
@@ -59,8 +59,47 @@ func (dll *Dll) PopRight(numPop ...int) interface{} {
 		if dll.length != 0 {
 			temp := dll.tail
 			dll.tail = dll.tail.prev
+			if dll.tail != nil {
+				dll.tail.next = nil
+			} else {
+				dll.head = nil // list becomes empty after this pop
+			}
 			dll.length--
+			temp.prev = nil
 			return temp.value
+		}
+		return nil
+	} else {
+		num := numPop[0]
+		poppedValues := make([]interface{}, 0, num)
+		if num > dll.length {
+			num = dll.length
+		}
+		for i := 0; i < num; i++ {
+			if dll.tail != nil {
+				poppedValues = append(poppedValues, dll.tail.value)
+				prev := dll.tail.prev
+				dll.tail.prev = nil
+				dll.tail = prev
+				if dll.tail != nil {
+					dll.tail.next = nil
+				} else {
+					dll.head = nil // list becomes empty
+				}
+				dll.length--
+			}
+		}
+		return poppedValues
+	}
+}
+
+func (dll *Dll) PopLeft(numPop ...int) interface{} {
+	head := dll.head
+	if len(numPop) == 0 {
+		if head != nil {
+			dll.head = head.next
+			dll.length--
+			return head.value
 		} else {
 			return nil
 		}
@@ -69,6 +108,17 @@ func (dll *Dll) PopRight(numPop ...int) interface{} {
 		if num > dll.length {
 			num = dll.length
 		}
-
+		poppedValues := make([]interface{}, 0, num)
+		for i := 0; i < num; i++ {
+			if head != nil {
+				poppedValues = append(poppedValues, head.value)
+				head = head.next
+				head.prev = nil	
+			} else {
+				dll.tail = nil
+			}
+			dll.length--
+		}
+		return poppedValues
 	}
 }

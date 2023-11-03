f15e15d3271d5dab55419a501c048343bbb312b9
Jay Reddy <jayreddy@gmail.com>
2023-11-03 07:04:59 -0700

    finishes PopBackOne() func and adds edge case to PushBackOne()

--- circular/deque.go
+++ circular/deque.go
@@ -1,57 +1,70 @@
 package circular
 
 type Deque struct {
-    data []interface{}
-    front int
-    back int
-    length int
-    capacity int
+	data     []interface{}
+	front    int
+	back     int
+	length   int
+	capacity int
 }
 
 func New(capacity int) *Deque {
-    return &Deque{
-        data: make([]interface{}, capacity),
-        front: 0,
-        back: 0,
-        length: 0,
-        capacity: capacity,
-    }
+	return &Deque{
+		data:     make([]interface{}, capacity),
+		front:    0,
+		back:     0,
+		length:   0,
+		capacity: capacity,
+	}
 }
 
 func (d *Deque) resize() {
-    newCapacity := 2 * d.capacity 
+	newCapacity := 2 * d.capacity
 
-    newData := make([]interface{}, newCapacity)
-    pos := 0
+	newData := make([]interface{}, newCapacity)
+	pos := 0
 
-    for i := 0; i < d.length; i++ {
-        newData[pos] = d.data[d.front]
-        d.front = (d.front + 1) % d.capacity
-        pos++
-    }
+	for i := 0; i < d.length; i++ {
+		newData[pos] = d.data[d.front]
+		d.front = (d.front + 1) % d.capacity
+		pos++
+	}
 
-    d.front = 0
-    d.back = pos
-    d.data = newData
-    d.capacity = newCapacity
+	d.front = 0
+	d.back = pos
+	d.data = newData
+	d.capacity = newCapacity
 }
 
-
 func (d *Deque) PushBackOne(v interface{}) {
-    if d.length >= d.capacity {
-        d.resize()
-    }
+	if d.length >= d.capacity {
+		d.resize()
+	}
 
-    pos := (d.back + 1) % d.capacity
-    d.data[pos] = v
-    d.length++
-    d.back = pos
+	if d.length != 0 {
+		d.back = (d.back + 1) % d.capacity
+	}
+
+	d.data[d.back] = v
+	d.length++
 }
 
 func (d *Deque) PopBackOne(v interface{}) interface{} {
-    if d.length == 0 {
-        return nil
-    }
+	if d.length == 0 {
+		return nil
+	}
 
-    
+	popped := d.data[d.back]
+    d.data[d.back] = nil
+	if d.length != 1 {
+		if d.back != 0 {
+			d.back--
+		} else {
+			d.back = d.capacity - 1
+		}
+	} else {
+		d.back, d.front = 0, 0
+	}
+    d.length--
+	return popped
 }
